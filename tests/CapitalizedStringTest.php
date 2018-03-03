adb610c41c4debeebfec8edc106141a80ea9a001
Andrew Cairns <andrew@acairns.co.uk>
2018-03-03 10:17:40 +0000

    The first character must be a capital

--- tests/CapitalizedStringTest.php
+++ tests/CapitalizedStringTest.php
@@ -7,6 +7,7 @@ class CapitalizedStringTest extends TestCase
     public function test_it_can_not_be_capitalized_if_empty()
     {
         $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('An empty string cannot be capitalized.');
 
         new CapitalizedString('');
     }
@@ -14,7 +15,16 @@ class CapitalizedStringTest extends TestCase
     public function test_it_can_not_start_with_a_number()
     {
         $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("The string '0Foo' is not capitalized");
 
         new CapitalizedString('0Foo');
+    }
+
+    public function test_the_first_character_must_be_a_capital()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("The string 'fOO' is not capitalized");
+
+        new CapitalizedString('fOO');
     }
 }
